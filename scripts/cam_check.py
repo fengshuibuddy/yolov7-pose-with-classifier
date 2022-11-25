00d2647d544d419dfeaaac088938f2d01c830f04
fengshuibuddy <aidyn.abirov@icloud.com>
2022-11-25 17:06:30 +0600

    Updated cam_check

--- scripts/cam_check.py
+++ scripts/cam_check.py
@@ -1,9 +1,24 @@
 import cv2
 
 cap = cv2.VideoCapture("rtsp://admin:New@ction2299@10.10.25.31/LiveMedia/ch1/Media1")
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+print(frame_width)
+print(frame_height)
+out = cv2.VideoWriter('walkpose.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20, (frame_width, frame_height))
 
 while cap.isOpened():
-
     ret, frame = cap.read()
+
+    out.write(frame)
+
     cv2.imshow('VIDEO', frame)
     cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
